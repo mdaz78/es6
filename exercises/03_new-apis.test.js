a9f3e44e2a218c74b742c46380d9c241b6f79c6e
Md Abu Zafar <mdabuzafar78@gmail.com>
2021-03-04 14:06:51 +0530

    Do the exercise for new_apis

--- exercises/03_new-apis.test.js
+++ exercises/03_new-apis.test.js
@@ -1,29 +1,33 @@
-test(`should be easier to determine whether a string includes another`, () => {
+test.only(`should be easier to determine whether a string includes another`, () => {
   const sentence = 'It was the best of times. It was the worst of times'
   // create a variable called `result` that is assigned to a call of sentence.includes
+  const result = sentence.includes('best')
   expect(result).toBe(true)
 })
 
-test(`should be easier to repeat a string`, () => {
+test.only(`should be easier to repeat a string`, () => {
   const repeated = 'abc123'
   // create a variable called `result` that is the result of repeating the string 4 times
+  const result = repeated.repeat(4)
   expect(result).toBe('abc123abc123abc123abc123')
 })
 
-test(`should be able to take an array-like object and convert it into an array`, () => {
+test.only(`should be able to take an array-like object and convert it into an array`, () => {
   const obj = {length: 3, 0: 'a', 1: 'b', 2: 'c'}
   // this is even more handy with a NodeList like that returned from document.querySelector
   // create a variable called `result` and assign it to a call to Array.from
+  const result = Array.from(obj)
   expect(result).toEqual(['a', 'b', 'c'])
 })
 
-test(`should be easier to fill an array with values`, () => {
+test.only(`should be easier to fill an array with values`, () => {
   const originalArray = new Array(5)
   // create a variable called `result` and assign it to an array that's filled with 3s except for the first item.
+  const result = originalArray.fill(3, 1, 5)
   expect(result).toEqual([, 3, 3, 3, 3]) // eslint-disable-line no-sparse-arrays
 })
 
-test(`should be easy to copy properties from one object to another`, () => {
+test.only(`should be easy to copy properties from one object to another`, () => {
   const source1 = {
     a: {
       b: 'c',
@@ -51,6 +55,7 @@ test(`should be easy to copy properties from one object to another`, () => {
     p: ['x', 'y', 'z'],
   }
   // merge the sources into the target using Object.assign
+  const result = Object.assign(target, source1, source2, source3)
 
   expect(result).toEqual({
     a: {
@@ -82,8 +87,8 @@ test(`should be easy to copy properties from one object to another`, () => {
 /*
 http://ws.kcd.im/?ws=ES6+and+Beyond&e=New+APIS&em=
 */
-test('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+test.only('I submitted my elaboration and feedback', () => {
+  const submitted = true // change this when you've submitted!
   expect(true).toBe(submitted)
 })
 ////////////////////////////////
