c0bf82c1df5e067e0c9dbe7f1350768f65beaf6c
Md Abu Zafar <mdabuzafar78@gmail.com>
2021-03-04 13:32:33 +0530

    Learn about destructuring

--- quizzes/04_destructure.js
+++ quizzes/04_destructure.js
@@ -3,9 +3,10 @@ import log from './pretty-log'
 function getAverage() {
   // refactor with object destructuring
   const obj = {x: 3.6, y: 7.8, z: 4.3}
+  const {x, y, z} = obj
   return Math.floor((obj.x + obj.y + obj.z) / 3.0)
 }
-// log(getAverage())
+log(getAverage())
 
 function getAvgTemp() {
   // refactor with nested destructuring
@@ -15,11 +16,19 @@ function getAvgTemp() {
     today: {max: 2.6, min: -6.3},
     tomorrow: {max: 3.2, min: -5.8},
   }
-  const maxToday = weather.today.max
-  const minToday = weather.today.min
 
-  const maxTomorrow = weather.tomorrow.max
-  const minTomorrow = weather.tomorrow.min
+  const {
+    unit,
+    today: {max: maxToday, min: minToday},
+    today,
+    tomorrow: {max: maxTomorrow, min: minTomorrow},
+  } = weather
+
+  // const maxToday = weather.today.max
+  // const minToday = weather.today.min
+
+  // const maxTomorrow = weather.tomorrow.max
+  // const minTomorrow = weather.tomorrow.min
 
   return {
     max: (maxToday + maxTomorrow) / 2.0,
@@ -27,28 +36,30 @@ function getAvgTemp() {
     unit: weather.unit,
   }
 }
-// log(getAvgTemp())
+log(getAvgTemp())
 
 function getFirstTwo() {
   // refactor with array destructuring
   const arr = [0, 1, 2, 3, 4, 5, 6, 7]
-  const firstItem = arr[0]
-  const secondItem = arr[1]
+  const [firstItem, secondItem] = arr
+  // const firstItem = arr[0]
+  // const secondItem = arr[1]
 
   return {
     firstItem: firstItem,
     secondItem: secondItem,
   }
 }
-// log(getFirstTwo())
+log(getFirstTwo())
 
 function getElements() {
   // returns 1st, 2nd and last element from an array
   // refactor with skipped destructuring for arrays
   const arr = [0, 1, 2, 3, 4, 5, 6, 7]
-  const first = arr[0]
-  const second = arr[1]
-  const fifth = arr[4]
+  const [first, second, , , fifth] = arr
+  // const first = arr[0]
+  // const second = arr[1]
+  // const fifth = arr[4]
 
   return {
     first: first,
@@ -56,7 +67,7 @@ function getElements() {
     fifth: fifth,
   }
 }
-// log(getElements())
+log(getElements())
 
 function getSecondItem() {
   // refactor with nested destructuring of arrays
@@ -65,9 +76,12 @@ function getSecondItem() {
     ['apple', 'mango', 'orange'],
     ['cookies', 'cake', 'pizza', 'chocolate'],
   ]
-  const firstItem = food[0][1]
-  const secondItem = food[1][1]
-  const thirdItem = food[2][1]
+
+  const [[, firstItem], [, secondItem], [, thirdItem]] = food
+
+  // const firstItem = food[0][1]
+  // const secondItem = food[1][1]
+  // const thirdItem = food[2][1]
 
   return {
     first: firstItem,
@@ -75,7 +89,7 @@ function getSecondItem() {
     third: thirdItem,
   }
 }
-// log(getSecondItem())
+log(getSecondItem())
 
 function nestedArrayAndObject() {
   // refactor this to a single line of destructuring...
@@ -91,37 +105,45 @@ function nestedArrayAndObject() {
       ],
     },
   }
-  const title = info.title
-  const protagonistName = info.protagonist.name
-  const enemy = info.protagonist.enemies[3]
-  const enemyTitle = enemy.title
-  const enemyName = enemy.name
+
+  const {
+    title,
+    protagonist: {
+      name: protagonistName,
+      enemies: [, , , {title: enemyTitle, name: enemyName}],
+    },
+  } = info
+
+  // const title = info.title
+  // const protagonistName = info.protagonist.name
+  // const enemy = info.protagonist.enemies[3]
+  // const enemyTitle = enemy.title
+  // const enemyName = enemy.name
   return `${enemyName} (${enemyTitle}) is an enemy to ${protagonistName} in "${title}"`
 }
-// log(nestedArrayAndObject())
+log(nestedArrayAndObject())
 
 function defaultValues() {
   const bench = {type: 'Piano', adjustable: false}
-  const legCount = bench.legs === undefined ? getDefaultLegCount() : bench.legs
+  const {legs: legCount = getDefaultLegCount()} = bench
   return legCount
 
   function getDefaultLegCount() {
     return 4
   }
 }
-// log(defaultValues())
+log(defaultValues())
 
 function ontoAnObject() {
   // refactor this to destructuring
   const array = [1, 2, 3, 4, 5, 6]
   const object = {}
-  object.one = array[0]
-  object.two = array[1]
-  object.three = array[2]
-  object.rest = array.slice(3)
+
+  ;[object.one, object.two, object.three, ...object.rest] = array
+
   return object
 }
-// log(ontoAnObject())
+log(ontoAnObject())
 
 /*
 
